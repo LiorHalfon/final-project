ab929ccf727a61846cbec659f4430e7857fefbff
LiorHalfon <lior.halfon2@gmail.com>
2017-06-07 02:37:29 +0300

    Added Login

--- web-server/src/main/java/servlets/flows/Login.java
+++ web-server/src/main/java/servlets/flows/Login.java
@@ -14,34 +14,25 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "CreateUser", urlPatterns = {"/createUser"})
-public class CreateUser extends HttpServlet {
-    private static final String FIRST_NAME = "firstname";
-    private static final String LAST_NAME = "lastname";
+@WebServlet(name = "Login", urlPatterns = {"/login"})
+public class Login extends HttpServlet {
     private static final String EMAIL = "email";
-    private static final String USER_ID = "userid";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        UserManager userMgr = ServletUtils.GetNewsWorkshopUtils(getServletContext()).userManager;
+        String email = request.getParameter(EMAIL);
+        User user = userMgr.findUserByEmail(email);
+        HttpSession session = request.getSession(true);
 
         PrintWriter out = response.getWriter();
-        UserManager userMgr = ServletUtils.GetNewsWorkshopUtils(getServletContext()).userManager;
-
-        String firstName = request.getParameter(FIRST_NAME);
-        String lastName = request.getParameter(LAST_NAME);
-        String email = request.getParameter(EMAIL);
-        String id = request.getParameter(USER_ID);
-        User user = new User(firstName,lastName,email);
-        user.id = id;
-
-        try {
-            userMgr.createUser(user);
-            HttpSession session = request.getSession(true);
+        if (user!=null){
             session.setAttribute(SessionUtils.USER_ID, user.id);
-
             out.write(user.firstName);
             response.setStatus(HttpServletResponse.SC_OK);
-        }catch (Exception e){
-            out.write("failed");
+        }
+        else
+        {
+            out.write("login failed");
             response.setStatus(HttpServletResponse.SC_OK);
         }
     }
