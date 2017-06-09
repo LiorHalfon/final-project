ea6713505fb1c2dbe85a3a632b478b46facb1cfb
Nir Dayan <ndayan@salesforce.com>
2017-06-09 10:07:29 +0300

    Add new project structure

---
+++ workshop-app/src/main/java/com/workshop/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.workshop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.workshop.model.Role;
+
+@Repository("roleRepository")
+public interface RoleRepository extends JpaRepository<Role, Integer>{
+	Role findByRole(String role);
+
+}
