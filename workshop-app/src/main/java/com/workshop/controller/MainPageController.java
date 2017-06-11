de60008483b18d771c70c88366cb12ab77066c96
LiorHalfon <lior.halfon2@gmail.com>
2017-06-11 13:17:08 +0300

    Connected search to the new app

--- workshop-app/src/main/java/com/workshop/controller/MainPageController.java
+++ workshop-app/src/main/java/com/workshop/controller/MainPageController.java
@@ -2,6 +2,9 @@ package com.workshop.controller;
 
 import com.workshop.model.User;
 import com.workshop.model.UserFeedback;
+import com.workshop.search.BingNewsFinder;
+import com.workshop.search.NewsFinder;
+import com.workshop.search.RelevantNews;
 import com.workshop.service.UserFeedbackService;
 import com.workshop.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by ndayan on 10/06/2017.
@@ -39,10 +45,45 @@ public class MainPageController {
     }
 
     @RequestMapping(value="/home/search", method = RequestMethod.GET)
-    public ModelAndView home(@Valid @ModelAttribute("queryValue") String queryValue) {
+    public ModelAndView home(@Valid @ModelAttribute("haveToAppear") String haveToAppearParam,
+                             @Valid @ModelAttribute("cantAppear") String cantAppearParam,
+                             @Valid @ModelAttribute("queryValue") String queryValueParam) {
         ModelAndView modelAndView = new ModelAndView();
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+
+        String[] data = haveToAppearParam.split(",");
+        ArrayList<String> haveToAppear = new ArrayList<>(Arrays.asList(data));
+        haveToAppear.remove("");
+
+        data = cantAppearParam.split(",");
+        ArrayList<String> cantAppear = new ArrayList<>(Arrays.asList(data));
+        cantAppear.remove("");
+
+        data = queryValueParam.split(",");
+        ArrayList<String> queries = new ArrayList<>(Arrays.asList(data));
+        queries.remove("");
+
+        ArrayList<String> blackListDomains = new ArrayList<>();
+        List<UserFeedback> userFB = userFeedbackService.getUserFeedbacks(user, UserFeedback.ActivityType.BLOCK_DOMAIN);
+        if (userFB != null) {
+            for (UserFeedback activity : userFB) {
+                if (activity.getActivityType().equals(UserFeedback.ActivityType.BLOCK_DOMAIN.toString())) {
+                    blackListDomains.add(activity.getUrl());
+                }
+            }
+        }
+
+        NewsFinder newsFinder = new BingNewsFinder();
+        newsFinder.Init(haveToAppear,cantAppear,blackListDomains,queries,user);
+        try {
+            newsFinder.Start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        List<RelevantNews> results = newsFinder.GetResults();
         modelAndView.setViewName("home");
 
         return modelAndView;
